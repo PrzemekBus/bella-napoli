67aa555164fab4597ae74bdaddc9f6f78d7ea308
pbus <gramp.fot@gmail.com>
2025-12-31 19:04:53 +0100

    add header section

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 import './styles/main.css'
+
+const burgerMenu = document.getElementById('burger');
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle('active');
+  document.querySelector('.header-nav').classList.toggle('active');
+});
