d7a06c30ca36622dea3f2c405c0077d5b5f52424
pbus <gramp.fot@gmail.com>
2026-01-02 00:09:53 +0100

    create hero section

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import './styles/main.css'
 
-const burgerMenu = document.getElementById('burger');
-burgerMenu.addEventListener('click', function() {
-  this.classList.toggle('active');
-  document.querySelector('.header-nav').classList.toggle('active');
-});
+const burgerMenu = document.getElementById('burger')
+burgerMenu.addEventListener('click', function () {
+	this.classList.toggle('active')
+	document.querySelector('.header-nav').classList.toggle('active')
+})
+
+const socialBar = document.querySelector('.header__social-bar')
+const navBar = document.querySelector('.header__nav')
+const SCROLL_HEIGHT = 0;
+
+window.addEventListener('scroll', () => {
+	socialBar.classList.toggle('scrolled', window.scrollY > SCROLL_HEIGHT)
+  navBar.classList.toggle('scrolled', window.scrollY > SCROLL_HEIGHT)
+})
